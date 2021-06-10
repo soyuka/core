3a9916d2ad64dad6cc2f4ab07b1af96a36567cd5
loicboursin <loic.boursin@gmail.com>
2021-06-10 16:53:53 +0200

    fix: started removing LegacyResourceNameCollectionFactoryInterface occurences

--- src/Metadata/Resource/Factory/CachedResourceNameCollectionFactory.php
+++ src/Metadata/Resource/Factory/CachedResourceNameCollectionFactory.php
@@ -22,7 +22,7 @@ use Psr\Cache\CacheItemPoolInterface;
  *
  * @author Teoh Han Hui <teohhanhui@gmail.com>
  */
-final class CachedResourceNameCollectionFactory implements LegacyResourceNameCollectionFactoryInterface
+final class CachedResourceNameCollectionFactory implements ResourceNameCollectionFactoryInterface
 {
     use CachedTrait;
 
@@ -39,10 +39,10 @@ final class CachedResourceNameCollectionFactory implements LegacyResourceNameCol
     /**
      * {@inheritdoc}
      */
-    public function create(bool $legacy = true): ResourceNameCollection
+    public function create(): ResourceNameCollection
     {
-        return $this->getCached($this->decorated instanceof LegacyResourceNameCollectionFactoryInterface ? sprintf('%s-%s', self::CACHE_KEY, $legacy) : self::CACHE_KEY, function () use ($legacy) {
-            return $this->decorated instanceof LegacyResourceNameCollectionFactoryInterface ? $this->decorated->create($legacy) : $this->decorated->create();
+        return $this->getCached(self::CACHE_KEY, function () {
+            return $this->decorated->create();
         });
     }
 }
