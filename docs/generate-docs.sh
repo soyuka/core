80a5dd15abc25633db25260d8b535bb17b1796cc
Hugo Nicolas <hugo.nicolas@les-tilleuls.coop>
2022-12-20 11:38:02 +0100

    feat: generate all references

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -4,9 +4,18 @@ for d in ./guide/*.php; do
     php src/generate-guide.php $d > pages/guide/$(basename $d .php).mdx
 done
 
-mkdir pages/reference/Metadata
-for d in ../src/Metadata/*.php; do
-    php src/generate-reference.php $d > pages/reference/Metadata/$(basename $d .php).mdx
-done
+find ../src -type d -exec bash -c '
+    for pathname do
+            mkdir -p pages/reference/"${pathname:7}"
+    done' bash {} +
+
+find ../src -type f -name "*.php" -exec bash -c '
+    for file do
+    file=${file:7}
+    length=${#file}
+    file=${file::length-4}
+            php src/generate-reference.php ../src/$file.php > pages/reference/${file}.mdx
+    done
+' bash {} +
 
 php src/generate-sidebar.php > ./pages/sidebar.mdx
