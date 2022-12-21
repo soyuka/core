b85e7329913a7d46e2cc214cff7339a70914101e
Hugo Nicolas <hugo.nicolas@les-tilleuls.coop>
2022-12-21 10:07:39 +0100

    feat: prevent internal classes to be referenced

--- docs/src/generate-references.php
+++ docs/src/generate-references.php
@@ -12,6 +12,13 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocChildNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\ConstExprParser;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TokenIterator;
+use PHPStan\PhpDocParser\Parser\TypeParser;
 use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Finder\Finder;
 
@@ -20,6 +27,29 @@ $config = (require('config.php'))();
 $root = Path::makeAbsolute($config['reference']['src'], getcwd());
 $patterns = $config['reference']['patterns'];
 $referencePath = $config['sidebar']['directories']['Reference'][0];
+
+$parser = new PhpDocParser(new TypeParser(new ConstExprParser()), new ConstExprParser());
+$lexer = new Lexer();
+
+function isInternal(ReflectionClass $class, Lexer $lexer, PhpDocParser $parser): bool
+{
+    $doc = $class->getDocComment();
+    if (!$doc) {
+        return false;
+    }
+    $tokens = new TokenIterator($lexer->tokenize($doc));
+    $phpDocNode = $parser->parse($tokens);
+    $tokens->consumeTokenType(Lexer::TOKEN_END);
+    $tags = array_filter($phpDocNode->children, static function (PhpDocChildNode $childNode): bool {
+        return $childNode instanceof PhpDocTagNode;
+    });
+    foreach ($tags as $tag) {
+        if ("@internal" === $tag->name) {
+            return true;
+        }
+    }
+    return false;
+}
 
 $files = [];
 foreach ($patterns['names'] as $pattern) {
@@ -37,6 +67,12 @@ foreach ($patterns['directories'] as $pattern) {
 foreach ($files as $file) {
     $relativeToSrc = Path::makeRelative($file->getPath(), $root);
     $relativeToDocs = Path::makeRelative($file->getRealPath(), getcwd());
+
+    $namespace = 'ApiPlatform\\'.str_replace(['/', '.php'], ['\\', ''], $relativeToSrc).'\\'.$file->getBasename('.php');
+    if (isInternal(new ReflectionClass($namespace), $lexer, $parser)) {
+        continue;
+    }
+
     exec('mkdir -p '.$referencePath.'/'.$relativeToSrc);
     exec('php src/generate-reference.php '.$relativeToDocs.' > '.$referencePath.'/'.$relativeToSrc.'/'.str_replace('.php','.mdx',$file->getBasename()));
 }
