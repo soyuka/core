f424451d127131d1a1aa751dab00a7b1a55dd8b3
Vincent Chalamon <vincentchalamon@protonmail.com>
2023-01-12 10:47:29 +0100

    fix: revamp fixtures + add tests

--- docs/guide/handle-a-pagination-on-a-custom-collection.php
+++ docs/guide/handle-a-pagination-on-a-custom-collection.php
@@ -5,11 +5,9 @@
 // executable: true
 // ---
 
-// In case you're using a custom collection (through a Provider), make sure you return the `Paginator` object to get the
-// full hydra response with `hydra:view` (which contains information about first, last, next and previous page).
+// In case you're using a custom collection (through a Provider), make sure you return the `Paginator` object to get the full hydra response with `hydra:view` (which contains information about first, last, next and previous page).
 //
-// The following example shows how to handle it using a custom Provider. You will need to use the Doctrine Paginator and
-// pass it to the API Platform Paginator.
+// The following example shows how to handle it using a custom Provider. You will need to use the Doctrine Paginator and pass it to the API Platform Paginator.
 
 namespace App\Entity {
     use ApiPlatform\Metadata\GetCollection;
@@ -17,7 +15,7 @@ namespace App\Entity {
     use App\State\BooksListProvider;
     use Doctrine\ORM\Mapping as ORM;
 
-    // Use custom Provider on operation to retrieve the custom collection
+    /* Use custom Provider on operation to retrieve the custom collection */
     #[GetCollection(provider: BooksListProvider::class)]
     #[ORM\Entity(repositoryClass: BookRepository::class)]
     class Book
@@ -49,7 +47,7 @@ namespace App\Repository {
 
         public function getPublishedBooks(int $page = 1, int $itemsPerPage = 30): DoctrinePaginator
         {
-            // Retrieve the custom collection and inject it into a Doctrine Paginator object
+            /* Retrieve the custom collection and inject it into a Doctrine Paginator object */
             return new DoctrinePaginator(
                 $this->createQueryBuilder('b')
                      ->where('b.published = :isPublished')
@@ -79,28 +77,21 @@ namespace App\State {
 
         public function provide(Operation $operation, array $uriVariables = [], array $context = []): Paginator
         {
-            // Retrieve the pagination parameters from the context thanks to the Pagination object
+            /* Retrieve the pagination parameters from the context thanks to the Pagination object */
             [$page, , $limit] = $this->pagination->getPagination($operation, $context);
 
-            // Decorates the Doctrine Paginator object to the API Platform Paginator one
+            /* Decorates the Doctrine Paginator object to the API Platform Paginator one */
             return new Paginator($this->bookRepository->getPublishedBooks($page, $limit));
         }
     }
 }
 
 namespace App\Playground {
-    use App\Kernel;
     use Symfony\Component\HttpFoundation\Request;
 
     function request(): Request
     {
         return Request::create('/books.jsonld', 'GET');
-    }
-
-    function setup(Kernel $kernel): void
-    {
-        $kernel->executeMigration();
-        $kernel->loadFixtures();
     }
 }
 
@@ -109,34 +100,45 @@ namespace DoctrineMigrations {
 
     final class Migration extends AbstractMigration
     {
-        public function getDescription(): string
-        {
-            return 'Creates Book objects.';
-        }
-
         public function up(): void
         {
             $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, is_published SMALLINT NOT NULL)');
-        }
-
-        public function down(): void
-        {
-            $this->addSql('DROP TABLE book');
         }
     }
 }
 
 namespace App\Fixtures {
     use App\Entity\Book;
+    use Doctrine\Bundle\FixturesBundle\Fixture;
     use Zenstruck\Foundry\AnonymousFactory;
 
-    final class BookFixtures
+    final class BookFixtures extends Fixture
     {
-        public function __invoke(): void
+        public function load(): void
         {
-            $factory = AnonymousFactory::new(Book::class);
-            $factory->many(20)->create(['published' => true]);
-            $factory->many(20)->create(['published' => false]);
+            /* Create 60 books randomly published or not */
+            AnonymousFactory::new(Book::class)::createMany(60, function () {
+                return [
+                    'published' => (bool) random_int(0, 1),
+                ];
+            });
+        }
+    }
+}
+
+namespace App\Tests {
+    use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+    use App\Entity\Book;
+
+    final class BookTest extends ApiTestCase
+    {
+        public function testAsAnonymousICanAccessTheDocumentation(): void
+        {
+            $response = static::createClient()->request('GET', '/books.jsonld');
+
+            $this->assertResponseIsSuccessful();
+            $this->assertMatchesResourceCollectionJsonSchema(Book::class, '_api_/books.{_format}_get_collection', 'jsonld');
+            $this->assertNotSame(0, $response->toArray(false)['hydra:totalItems'], 'The collection is empty.');
         }
     }
 }
