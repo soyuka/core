f8ddf3ba72fcffeb5145f1108260f68f3780312a
soyuka <soyuka@users.noreply.github.com>
2024-04-02 09:39:50 +0200

    fix(validator): validation exception route name

--- src/Validator/Exception/ValidationException.php
+++ src/Validator/Exception/ValidationException.php
@@ -20,7 +20,47 @@ use ApiPlatform\Metadata\Exception\RuntimeException;
  *
  * @author Kévin Dunglas <dunglas@gmail.com>
  */
+<<<<<<< Updated upstream
 class ValidationException extends RuntimeException
+=======
+#[ErrorResource(
+    uriTemplate: '/validation_errors/{id}',
+    status: 422,
+    openapi: false,
+    uriVariables: ['id'],
+    provider: 'api_platform.validator.state.error_provider',
+    shortName: 'ConstraintViolationList',
+    operations: [
+        new ErrorOperation(
+            name: '_api_validation_errors_problem',
+            routeName: 'api_validation_errors',
+            outputFormats: ['json' => ['application/problem+json']],
+            normalizationContext: ['groups' => ['json'],
+                'skip_null_values' => true,
+                'rfc_7807_compliant_errors' => true,
+            ]),
+        new ErrorOperation(
+            name: '_api_validation_errors_hydra',
+            routeName: 'api_validation_errors',
+            outputFormats: ['jsonld' => ['application/problem+json']],
+            links: [new Link(rel: 'http://www.w3.org/ns/json-ld#error', href: 'http://www.w3.org/ns/hydra/error')],
+            normalizationContext: [
+                'groups' => ['jsonld'],
+                'skip_null_values' => true,
+                'rfc_7807_compliant_errors' => true,
+            ]
+        ),
+        new ErrorOperation(
+            name: '_api_validation_errors_jsonapi',
+            routeName: 'api_validation_errors',
+            outputFormats: ['jsonapi' => ['application/vnd.api+json']],
+            normalizationContext: ['groups' => ['jsonapi'], 'skip_null_values' => true, 'rfc_7807_compliant_errors' => true]
+        ),
+    ],
+    graphQlOperations: []
+)]
+class ValidationException extends RuntimeException implements ConstraintViolationListAwareExceptionInterface, \Stringable, ProblemExceptionInterface, HttpExceptionInterface, SymfonyHttpExceptionInterface
+>>>>>>> Stashed changes
 {
     public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null, protected readonly ?string $errorTitle = null)
     {
